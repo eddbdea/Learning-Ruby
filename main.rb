2c61fa80670833f6d06a98c107c5afbd50e841d5
Eduard <eduardbadea@outlook.com>
2025-10-06 01:10:22 +0300

    numbers and strings

--- main.rb
+++
@@ -1,8 +0,0 @@
-puts "Introduce a number: "
-n = gets.chomp.to_i
-
-for i in 0..n
-    if i % 2 == 0
-        puts "I have #{i} dogs"
-    end
-end
---
+++ numbers.rb
@@ -0,0 +1,18 @@
+# Exponent
+puts 2 ** 2 # -> 4
+puts 4 ** 4 # -> 256
+
+# Modulus
+puts 8 % 2 # -> 0
+puts 9 % 2 # -> 1
+
+# Converting INT to FLOAT and FLOAT to INT
+puts 13.to_f # -> 13.0
+puts 13.4.to_i # -> 13
+
+# Useful methods to see if a number is EVEN or ODD
+puts 6.even? # -> returns TRUE
+puts 7.even? # -> returns FALSE
+
+puts 6.odd? # -> returns FALSE
+puts 7.odd? # -> returns TRUE
