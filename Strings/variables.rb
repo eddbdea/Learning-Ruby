0bb6b214c14605f30033204ac3033f6dd97223a1
Eduard <eduardbadea@outlook.com>
2025-10-13 08:51:24 +0300

    declaring variables with strings

---
+++ Strings/variables.rb
@@ -0,0 +1,15 @@
+string_a = "hello"
+string_b = string_a
+puts string_a # -> hello
+puts string_b# -> hello
+
+puts string_a.upcase # -> HELLO
+puts string_b # -> hello
+
+puts string_a.upcase! # -> HELLO
+puts string_b # -> HELLO -> because string_b holds a reference that points towards "hello" object
+
+#if we want to copy a string inside another variable by assigning the string object variable that holds the object reference
+#to make sure it doesnt change if the object is mutated, we can create a copy
+
+puts string_b = string_a.dup # -> creates a new string with the value of string_aß
